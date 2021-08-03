eb6089a351e2e0cb2f2cf43e1bc5b23ca6ed5301
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-04 01:21:09 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -66,6 +66,11 @@ async def on_message(message):
 
     if(message.content == "!시간"):
         await message.channel.send(embed=discord.Embed(title="Time", timestamp=datetime.datetime.utcnow()))
+        
+    if content.startswith("!명령어"):
+        embed=discord.Embed(description="<<명령어>>", color=0x00ff56)
+        embed.set_author(name="!준민이 뺨따꾸 때리기\n!인백이의 드립은\n!자기소개\n!시간\n!시간표\n!월시간표\n!화시간표\n!수시간표\n!목시간표\n!금시간표")
+        await message.channel.send(embed=embed)
 
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
