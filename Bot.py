5ba4e6302d1b6fb12f23fab3d7a8c570ce34ead0
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-21 16:50:58 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -193,7 +193,6 @@ async def on_message(message):
 
     if content.startswith("!레시피"):
         embed=discord.Embed(description="```fix\n힐링포션```\n난초 + 유리병\n난초 = 약초 + 꽃\n========================\n```fix\n구급상자```\n지혈제 + 붕대\n지혈제 = 알코올 + 붕대\n========================\n```fix\n생선튀김```\n붕어 + 뜨거운 오일\n뜨거운 오일 = 오일 + 라이터\n========================\n```fix\n감자튀김```\n감자 + 뜨거운 오일\n뜨거운 오일 = 오일 + 라이터\n========================\n```fix\n피쉬앤칩스```\n감자튀김 + 생선튀김\n========================\n```fix\n햄버거```\n고기 + 빵\n========================\n```fix\n감자튀김```\n감자 + 뜨거운 오일\n뜨거운오일 = 라이터 + 오일\n========================\n```fix\n일레븐세트```\n햄버거 + 감자튀김\n========================", color=0x00ff56)
-        embed.set_author(name=" ")
         await message.channel.send(embed=embed)
         
 access_token = os.environ["BOT_TOKEN"]
