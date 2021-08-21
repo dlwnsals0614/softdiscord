34b9f90c7e0ffbfd4684d9136a00cb7480eec736
won_chan <83020544+boychan112@users.noreply.github.com>
2021-08-21 17:07:51 +0900

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -165,11 +165,13 @@ async def on_message(message):
     if content.startswith("!옵지"):
         nick = message.content[4:]
         embed=discord.Embed(description="오피지지 {} 바로가기\nhttps://www.op.gg/summoner/userName={}".format(nick,nick), color=0x00ff56)
+        embed.set_author(name="<<{}>>".format(nick))
         await message.channel.send(embed=embed)
         
     if content.startswith("!롤체"):
         nick = message.content[4:]
         embed=discord.Embed(description="롤체지지 {} 바로가기\nhttps://lolchess.gg/profile/kr/{}".format(nick,nick), color=0x00ff56)
+        embed.set_author(name="<<{}>>".format(nick))
         await message.channel.send(embed=embed)
         
     if content.startswith("!닥지"):
